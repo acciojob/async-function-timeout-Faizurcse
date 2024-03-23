1f9cd3e412cd732263bec4854eccb99e117d4d2a
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-03-23 15:31:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let btn = document.getElementById('btn')
+btn.addEventListener('click',()=>{
+	let text = document.getElementById('text')
+	let number = document.getElementById('delay')
+	const p1 = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve(`${text.value}`)
+	},`${(number.value)*1000}`)
+})
+	async function getdata(){
+		document.getElementById('output').innerText = await p1;
+	}
+	getdata();
+})

b80712f1881086fc88268e52050099cb28cd6dac
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-03-23 15:38:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 //your JS code here. If required.
-let btn = document.getElementById('btn')
-btn.addEventListener('click',()=>{
-	let text = document.getElementById('text')
-	let number = document.getElementById('delay')
-	const p1 = new Promise((resolve,reject)=>{
-	setTimeout(()=>{
-		resolve(`${text.value}`)
-	},`${(number.value)*1000}`)
-})
-	async function getdata(){
-		document.getElementById('output').innerText = await p1;
-	}
-	getdata();
-})
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', async () => {
+  let text = document.getElementById('text').value;
+  let delay = document.getElementById('delay').value;
+
+  const delayedMessage = async () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(text);
+      }, delay * 1000);
+    });
+  };
+
+  try {
+    const message = await delayedMessage();
+    document.getElementById('output').innerText = message;
+  } catch (error) {
+    console.error(error);
+  }
+});
